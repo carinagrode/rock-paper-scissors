4fe491f9ae252131e8b4f2929edb08791d7422a3
Carina Grode <106753609+carinagrode@users.noreply.github.com>
2023-04-22 09:22:44 +0200

    Write the variables, functions and test the game

--- script.js
+++ script.js
@@ -15,6 +15,108 @@ function getComputerChoice() {
 }
 
 // Ask user for choice: rock, paper or scissors
+// Make the choice case-insensitive, so you can enter rock, Rock or ROCK
+function getUserChoice() {
+    let enteredUserChoice = prompt('Choose wisely: rock, paper or scissors?');
+    let lowerCaseUserChoice = enteredUserChoice.toLowerCase();
+
+    if (lowerCaseUserChoice == 'rock' || lowerCaseUserChoice == 'paper' || lowerCaseUserChoice == 'scissors') {
+        return lowerCaseUserChoice;
+    } else {
+        enteredUserChoice = prompt('Please choose one of these: rock, paper or scissors?');
+        lowerCaseUserChoice = enteredUserChoice.toLowerCase();
+        return lowerCaseUserChoice;
+    }
+}
+
+function playRound() {
+    // Compare the computerChoice with the userChoice
+    // Display the result
+
+    let computerChoice = getComputerChoice();
+    let userChoice = getUserChoice();
+
+    if (computerChoice === 'rock' && userChoice === 'rock') {
+        alert(`You both chose rock. Undecided! Score: ${userScore} : ${computerScore}`);
+    } else if (computerChoice === 'paper' && userChoice === 'paper') {
+        alert(`You both chose paper. Undecided! Score: ${userScore} : ${computerScore}`);
+    } else if (computerChoice === 'scissors' && userChoice === 'scissors') {
+        alert(`You both chose scissors. Undecided! Score: ${userScore} : ${computerScore}`);
+    } else if (computerChoice === 'rock' && userChoice === 'scissors') {
+        computerScore++;
+        alert(`You chose scissors, the computer chose rock. You lose! Score: ${userScore} : ${computerScore}`);
+    } else if (computerChoice === 'rock' && userChoice === 'paper') {
+        userScore++;
+        alert(`You chose paper, the computer chose rock. You win! Score: ${userScore} : ${computerScore}`);
+    } else if (computerChoice === 'scissors' && userChoice === 'rock') {
+        userScore++;
+        alert(`You chose rock, the computer chose scissors. You win! Score: ${userScore} : ${computerScore}`);
+    } else if (computerChoice === 'scissors' && userChoice === 'paper') {
+        computerScore++;
+        alert(`You chose paper, the computer chose scissors. You lose! Score: ${userScore} : ${computerScore}`);
+    } else if (computerChoice === 'paper' && userChoice === 'rock') {
+        computerScore++;
+        alert(`You chose rock, the computer chose paper. You lose! Score: ${userScore} : ${computerScore}`);
+    } else if (computerChoice === 'paper' && userChoice === 'scissors') {
+        userScore++;
+        alert(`You chose scissors, the computer chose paper. You win! Score: ${userScore} : ${computerScore}`);
+    } else {
+        alert(`Something went wrong. Refresh and try again! Score: ${userScore} : ${computerScore}`);
+    }
+}
+
+function playGame(finalScore) {
+
+    while (computerScore < finalScore && userScore < finalScore) {
+        playRound();
+    }
+
+    if (computerScore === finalScore) {
+        alert(`You lose! The computer has first reached ${finalScore} points!`);
+    } else {
+        alert(`You win! You have first reached ${finalScore} points!`);
+    }
+    
+}
+
+let finalScore = parseInt(prompt('Welcome to Rock, Paper, Scissors! Choose a final score between 1 and 5.'));
+
+let computerScore = 0;
+let userScore = 0;
+
+
+playGame(finalScore);
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// Computer chooses randomly between rock, paper, scissors
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    let randomChoice;
+    
+    if (randomNumber === 0) {
+        randomChoice = 'rock';
+    } else if (randomNumber === 1) {
+        randomChoice = 'paper';
+    } else {
+        randomChoice = 'scissors';
+    }
+
+    return randomChoice;
+}
+
+// Ask user for choice: rock, paper or scissors
 function getUserChoice() {
     let enteredUserChoice = prompt('Choose wisely: rock, paper or scissors?');
 
@@ -54,4 +156,4 @@ if (computerChoice === 'rock' && userChoice === 'rock') {
     alert('Something went wrong. Refresh and try again!');
 }
 
-
+*/
